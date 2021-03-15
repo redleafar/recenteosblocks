93dedecc2c02d390697407e908527b756329a782
Rafael Bermudez <rbermudez@rccl.com>
2021-03-15 11:54:42 -0500

    Last changes

--- app/src/main/java/one/block/recenteosblocks/ui/list/ListViewModel.kt
+++ app/src/main/java/one/block/recenteosblocks/ui/list/ListViewModel.kt
@@ -5,15 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.gson.JsonObject
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import one.block.recenteosblocks.data.db.entities.Block
 import one.block.recenteosblocks.data.db.entities.BlockchainInfo
 import one.block.recenteosblocks.data.repositories.BlockRepository
 import one.block.recenteosblocks.util.Constants.BLOCK_NUM_OR_ID
 import one.block.recenteosblocks.util.Constants.NUMBER_OF_BLOCKS
-import one.block.recenteosblocks.util.Coroutines
 import one.block.recenteosblocks.util.Event
 import one.block.recenteosblocks.util.getRequestBody
 import java.io.IOException
@@ -42,9 +42,10 @@ class ListViewModel(
     fun getBlockchainInfo() {
         viewModelScope.launch {
             try {
-                val blockChainInfo = viewModelScope.async {
-                    blockRepository.getBlockchainInfo()
-                }.await()
+                val blockChainInfo =
+                    withContext(viewModelScope.coroutineContext) {
+                        blockRepository.getBlockchainInfo()
+                    }
                 _blockchainInfo.value = blockChainInfo
                 getBlocksList()
             } catch (networkException: IOException) {
@@ -65,7 +66,7 @@ class ListViewModel(
 
     private fun getLastNBlocks(numBlocks: Int, requestBody: JsonObject) {
         if (numBlocks > 0) {
-            viewModelScope.launch {
+            viewModelScope.launch(Dispatchers.IO) {
                 try {
                     val block = viewModelScope.async {
                         blockRepository.getBlock(requestBody)
@@ -80,7 +81,7 @@ class ListViewModel(
 
     private fun saveBlockAndGetPrevious(numBlocks: Int, block: Block?) {
         block?.let {
-            _block.value = it
+            _block.postValue(it)
             viewModelScope.launch {
                 blockRepository.saveBlock(it)
             }
